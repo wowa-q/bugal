60c598f863e536ddca5911a453a251aa0b15d8c5
wakl8754 <waldemar.kloos@elektrobit.com>
2023-04-14 10:06:18 +0200

    test setup with pytest and fixtures

---
+++ bugal/cli.py
@@ -0,0 +1,21 @@
+def provide ():
+    return True
+
+def provide_list():
+    month_data_header = [
+        'id',
+        'date',
+        'booking-date',
+        'text',
+        'debitor',
+        'verwendung',
+        'konto',
+        'blz',
+        'value',
+        'debitor-id',
+        'mandat',
+        'customer',
+        'class',
+        'category'
+    ]
+    return month_data_header
---
+++ tests/conftest.py
@@ -0,0 +1,6 @@
+# pylint: skip-file
+import pathlib
+
+from fixtures.basic import fx_month_data
+
+FIXTURE_DIR = pathlib.Path(__file__).resolve() / "fixtures"
