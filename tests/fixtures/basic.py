297de5df235e8d687f1faf1fe0806b0487f39467
wowa-q <waldemar.kloos@gmail.com>
2023-04-16 22:42:43 +0200

    fixtures cleanup

--- tests/fixtures/basic.py
+++ tests/fixtures/basic.py
@@ -10,7 +10,7 @@ from datetime import datetime
 
 # 3rd party
 import pytest
-from openpyxl import Workbook
+# from openpyxl import Workbook
 
 
 FIXTURE_DIR = pathlib.Path(__file__).parent.resolve()
@@ -34,6 +34,55 @@ def fx_transactions_list_example():
     data = ["2022-01-01", "2022-01-01", "text", "debitor", "verwendung", "konto", "blz", 10, "debitor_id", "mandats_ref", "customer_ref", "checksum", "src_konto"]
     transactions.append(data)
     return transactions
+
+@pytest.fixture
+def fx_single_csv():
+    """Definitiaon of csv file, which shall be created and deleted when the test was done
+
+    Yields:
+        path string: file path
+    """
+    pth = ''
+    with open(FIXTURE_DIR / 'single.csv', 'w') as f:
+        f.write('a,b,c')
+        pth = FIXTURE_DIR / 'single.csv'
+
+    yield pth 
+    # delete the modified db file and copy one to make repeat of the test possible
+    try:
+        pth.unlink()
+    except FileNotFoundError:
+        pass
+    
+    return pth
+
+@pytest.fixture
+def fx_banch_of_csv():
+    pth = FIXTURE_DIR / 'csv'
+    try:
+        pth.mkdir()
+        for n in range(3):
+            name = 'single_'+str(n)+'.csv'
+            with open(pth / name, 'w') as f:
+                f.write('a,b,c')
+    except FileExistsError:
+        pass  
+
+    yield pth
+
+    try:
+        flist = pth.glob('*.csv')
+        for f in flist:
+            f.unlink()
+        pathlib.Path(pth).rmdir()
+    except PermissionError:
+        pass
+
+@pytest.fixture
+def fx_export_filter_aggregate():
+    fil = None
+    return fil
+
 
 @pytest.fixture
 def fx_xls_file2create():
@@ -78,11 +127,11 @@ def fx_month_data():
     #     month_data.append(month_data_row)
     return month_data_header
 
-@pytest.fixture
-def fx_xls_template(fx_xls_file2create):
-    wb = Workbook(write_only=True)
-    ws = wb.create_sheet()
-    ws.title='Jan'
-    for irow in range(10):
-        ws.append(['%d' % i for i in range(12)])
-    wb.save(fx_xls_file2create)
+# @pytest.fixture
+# def fx_xls_template(fx_xls_file2create):
+#     wb = Workbook(write_only=True)
+#     ws = wb.create_sheet()
+#     ws.title='Jan'
+#     for irow in range(10):
+#         ws.append(['%d' % i for i in range(12)])
+#     wb.save(fx_xls_file2create)
